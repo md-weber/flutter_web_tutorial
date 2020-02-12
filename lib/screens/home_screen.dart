55edad39ede15280b5ff2978d9b0e81a332a76aa
myracledesign <weber.max.1988@live.de>
2020-02-12 23:16:28 +0100

    added frank and Icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../frank_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello Youtube to Flutter Explained!"),
+        title: Text("Flutter Explained"),
       ),
       body: Container(
         child: Center(
-          child: Text(
-            "Flutter Explained",
-            style: TextStyle(fontSize: 102.0),
+          child: ListView(
+            children: <Widget>[
+              Text(
+                "Welcome on that page!",
+                style: TextStyle(fontSize: 64.0),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Thanks for reading the article or watching the video!",
+                style: TextStyle(fontSize: 54.0),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: FrankLoadingSpinner(
+                  bodyColor: Colors.lightBlue,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Feather.youtube),
+                    color: Color(0xFFFF0000),
+                    onPressed: () {
+                      _launchURL(
+                        "https://www.youtube.com/watch?v=iHIhnALkFj0&list=PLq83k-ITj6lQyw2sCawoRNnSGKZYuuUYh",
+                      );
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Feather.twitter),
+                    color: Colors.lightBlue,
+                    onPressed: () {
+                      _launchURL(
+                        "https://twitter.com/max_myracle",
+                      );
+                    },
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+_launchURL(url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
